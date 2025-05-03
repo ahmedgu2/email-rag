27c5528a09cfbb1f28fdc555d8355583d2af424e
Ahmed Gueddiche <ahmed.gueddiche2@gmail.com>
2025-05-02 20:38:42 -0400

    refactored + added date_time property

--- src/data_ingestion/create_collection.py
+++ src/data_ingestion/create_collection.py
@@ -1,30 +1,31 @@
 import weaviate
 import weaviate.classes.config as wc
 
-client = weaviate.connect_to_local()
+if __name__ == "__main__":
 
-emails = client.collections.create(
-    name="Emails",
-    properties=[
-        wc.Property(name='email_id', data_type=wc.DataType.TEXT, skip_vectorization=True),
-        wc.Property(name='thread_id', data_type=wc.DataType.TEXT, skip_vectorization=True),
-        wc.Property(name='sender_name', data_type=wc.DataType.TEXT),
-        wc.Property(name='sender_email', data_type=wc.DataType.TEXT),
-        wc.Property(name='receiver_name', data_type=wc.DataType.TEXT),
-        wc.Property(name='receiver_email', data_type=wc.DataType.TEXT),
-        wc.Property(name='subject', data_type=wc.DataType.TEXT),
-        wc.Property(name='body', data_type=wc.DataType.TEXT)
-    ],
-    vectorizer_config=wc.Configure.Vectorizer.text2vec_ollama(
-        api_endpoint="http://host.docker.internal:11434",
-        model='nomic-embed-text'
-    ),
-    generative_config=wc.Configure.Generative.ollama(
-        api_endpoint="http://host.docker.internal:11434",
-        model='llama3.2'
+    client = weaviate.connect_to_local()
+
+    emails = client.collections.create(
+        name="Emails",
+        properties=[
+            wc.Property(name='email_id', data_type=wc.DataType.TEXT, skip_vectorization=True),
+            wc.Property(name='thread_id', data_type=wc.DataType.TEXT, skip_vectorization=True),
+            wc.Property(name='sender_name', data_type=wc.DataType.TEXT),
+            wc.Property(name='sender_email', data_type=wc.DataType.TEXT),
+            wc.Property(name='receiver_name', data_type=wc.DataType.TEXT),
+            wc.Property(name='receiver_email', data_type=wc.DataType.TEXT),
+            wc.Property(name='date_time', data_type=wc.DataType.DATE),
+            wc.Property(name='subject', data_type=wc.DataType.TEXT),
+            wc.Property(name='body', data_type=wc.DataType.TEXT)
+        ],
+        vectorizer_config=wc.Configure.Vectorizer.text2vec_ollama(
+            api_endpoint="http://host.docker.internal:11434",
+            model='nomic-embed-text'
+        ),
+        generative_config=wc.Configure.Generative.ollama(
+            api_endpoint="http://host.docker.internal:11434",
+            model='llama3.2'
+        )
     )
-)
 
-print(emails)
-
-client.close()
+    client.close()
